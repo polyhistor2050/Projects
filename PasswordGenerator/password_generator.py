0927f59d2a091527dc4943141cd0731126fb5995
polyhistor2050 <chrisspaul2050@gmail.com>
2023-03-06 22:58:46 +0300

    added file

---
+++ PasswordGenerator/password_generator.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+adjectives  = ["sleepy", "slow", "smelly", 
+                "wet", "fat", "red", "red",
+                "orange", "yellow", "green",
+                "blue", "purple", "fluffy",
+                "white", "proud", "brave"]
+
+noun = ["apple", "dinosaur", "ball",
+        "toaster", "goat", "dragon",
+        "hammer", "duck", "panda"]
+
+print("Welcome to strong password generator!")
+
+adjectives = random.choice(adjectives)
+noun = random.choice(noun)
+number = random.randrange(0, 100)
+special_char = random.choice(string.punctuation)
+
+password = adjectives + noun + str(number) + special_char
+print("Your new password is: %s" % password)
